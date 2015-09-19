c4af0972f6a1e9770228549e09b30f61db503eb8
Iverson <lifejoice@outlook.com>
2015-09-19 18:47:29 +0800

    从原来的web_common_library迁移过来，并拆分出gaea-common和gaea-common-db。并转换为maven项目。 类命名为UR，还没改过来。下一步把类重命名了。
    History:
    
    2014-6-29
    
    1. UR_framework剥离校验、异常等通用代码，改为独立包(web_common_lib)导入使用。
    2. 创建新的web_common_lib，放置通用的代码，如果异常、util、校验等。
    
    2014-12-8
    
    临时提交版本。迁移mac的时候的中间提交。

---
+++ src/main/java/com/ur/framework/util/HibernateBaseDAO.java
@@ -0,0 +1,35 @@
+package com.ur.framework.util;
+
+import com.ur.framework.paging.PagingBean;
+import com.ur.framework.paging.PagingResultDataWrapper;
+import com.ur.framework.system.factory.WebDataWrapperFactory;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * 基于Hibernate的BaseDAO。提供基于Hibernate的公用方法。
+ * @author Iverson
+ * @history 2014年9月4日 星期四
+ */
+public class HibernateBaseDAO<T> {
+    @Autowired
+    private SessionFactory sessionFactory;
+    
+    protected List<T> query(String hql) {
+        Query query = sessionFactory.getCurrentSession().createQuery(hql);
+        List<T> list = query.list();
+        return list;
+    }
+    
+    protected PagingResultDataWrapper pagingQuery(String hql,PagingBean paging) {
+        Query query = sessionFactory.getCurrentSession().createQuery(hql);
+        query.setFirstResult(paging.getStart());
+        query.setMaxResults(paging.getPageSize());
+        List<T> list = query.list();
+        PagingResultDataWrapper result = WebDataWrapperFactory.pagingData(list);
+        return result;
+    }
+}
