59a6ec1573737d4b87de0b7edbc55630a11f9acf
iverson <lifejoice@outlook.com>
2016-01-31 17:28:43 +0800

    修改IdGenerator，默认是5位随机数，从26个字母+数字中随机抽取。不含特殊字符。  因为考虑到特殊字符中的\等在java中是有特殊定义，单引号、双引号在js也有特殊含义。避免程序以后发生不可预测的错误。宁愿多一位随机数。

--- src/main/java/org/gaea/extend/hibernate/id/GaeaDateTimeIDGenerator.java
+++ src/main/java/org/gaea/extend/hibernate/id/GaeaDateTimeIDGenerator.java
@@ -21,14 +21,15 @@ import java.util.Properties;
  * 这是一个自定义的ID生成器。<p/>
  * 依赖Hibernate。因为发现JPA和Spring Data貌似都没有自定义的ID生成器。<p/>
  * 规则：<br/>
- * 以年月日时分+一定位数的随机数为ID。默认是4位随机数，从ASCII的33-126中随机抽取。不含空格。
- * <p>输出ID格式：1601301620aX_4</p>
+ * 以年月日时分+一定位数的随机数为ID。默认是5位随机数，从26个字母+数字中随机抽取。不含特殊字符。<br/>
+ * 因为考虑到特殊字符中的\等在java中是有特殊定义，单引号、双引号在js也有特殊含义。避免程序以后发生不可预测的错误。宁愿多一位随机数。
+ * <p>输出ID格式：1601301620aXe54</p>
  * Created by iverson on 2016/1/30.
  */
 public class GaeaDateTimeIDGenerator implements IdentifierGenerator, Configurable {
     private final Logger logger = LoggerFactory.getLogger(GaeaDateTimeIDGenerator.class);
     private Integer randomLength = null;    // ID中随机生成数的位数
-    private static final Integer DEFAULT_RANDOM_LENGTH = 4;
+    private static final Integer DEFAULT_RANDOM_LENGTH = 5;
 
     public void configure(Type type, Properties properties, Dialect dialect) throws MappingException {
         String length = properties.getProperty("randomLength");
@@ -50,7 +51,7 @@ public class GaeaDateTimeIDGenerator implements IdentifierGenerator, Configurabl
                 // 生成ID以年月日时分开始。不到秒。
                 String dateTime = DateFormatUtils.format(new Date(), "yyMMddHHmm");
                 // 生成随机码，从ASCII码的第33位到126位作为随机字符池。ASCII 32位是空格，不能作为随机字符池。
-                String randomNum = RandomStringUtils.random(4, 33, 126, false, false);
+                String randomNum = RandomStringUtils.random(5, false, false);
                 return dateTime + randomNum;
             }
             return null;
